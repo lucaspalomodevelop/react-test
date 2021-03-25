83fa9ae199b7ed5b1a4e57a6df1cff969eb1fc17
lucaspalomodevelop <lucas.palomo@t-online.de>
2021-03-25 20:54:06 +0100

    ADJUSTMENTS

---
+++ src/components/dashboard/dashboard.js
@@ -0,0 +1,15 @@
+import './dashboard.css';
+
+function dashboard() {
+  return (
+    <div className="dashboard">
+      <header className="dashboard-header">
+        <p>
+        dashboard
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default dashboard;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Dashboard from './components/dashboard/dashboard';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Dashboard />
   </React.StrictMode>,
   document.getElementById('root')
 );
